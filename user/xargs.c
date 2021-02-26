d24f5457600471dfc46091c4087371840e83ac02
Kevin-Mei <Mei_Yao_1@163.com>
2021-02-26 20:40:40 +0800

    提交xargs题解

---
+++ user/xargs.c
@@ -0,0 +1,42 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    // 第一次传进来的xagrs echo bye
+    // 然后要从标准输入中读取数据，直到遇到\n
+    char buff[1024];
+    int offset = 0;
+    int len = read(0, buff+offset, sizeof(buff)-offset);
+    while(len)
+    {
+        // 判断遇到\n
+        if('\n' == buff[offset+len-1])
+        {
+            break;
+        }
+        len = read(0, buff+offset, sizeof(buff)-offset);
+    }
+    // 把buff添加进argv[], 然后exec
+    // 这里抄下echo的作业，不行，应该是fork，然后子进程执行argv[1]，然后argv[2]及以后的数据+buff是新的argv
+    int pid = fork();
+    if(pid < 0)
+    {
+        exit();
+    }else if(0 == pid)
+    {
+        int i =2;
+        while(i +1 < argc)
+        {
+            argv[i] = argv[i+1];
+            i++;
+        }
+
+        exec(argv[1], argv[2]);
+    }else
+    {
+        wait();
+    }
+
+    exit();
+}
