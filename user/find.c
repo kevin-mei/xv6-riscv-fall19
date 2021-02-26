6d055be6d75b6af6017700a1dbeaa530c1db5eff
Kevin-Mei <Mei_Yao_1@163.com>
2021-02-26 14:14:58 +0800

    更新lab1 find.c文件

--- user/find.c
+++ user/find.c
@@ -4,65 +4,71 @@
 #include "kernel/fs.h"
 
 char buf[1024];
-int match(char*, char*);
+int match(char *, char *);
 
-char*
+char *
 fmtname(char *path)
 {
-  static char buf[DIRSIZ+1];
+  static char buf[DIRSIZ + 1];
   char *p;
 
   // Find first character after last slash.
-  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+  for (p = path + strlen(path); p >= path && *p != '/'; p--)
     ;
   p++;
 
   // Return blank-padded name.
-  if(strlen(p) >= DIRSIZ)
+  if (strlen(p) >= DIRSIZ)
     return p;
   memmove(buf, p, strlen(p));
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  memset(buf + strlen(p), ' ', DIRSIZ - strlen(p));
   return buf;
 }
 
-void find(char *path, char* pattern)
+void find(char *path, char *pattern)
 {
   char buf[512], *p;
   int fd;
   struct dirent de;
   struct stat st;
 
-  if((fd = open(path, 0)) < 0){
+  if ((fd = open(path, 0)) < 0)
+  {
     fprintf(2, "find: cannot open %s\n", path);
     return;
   }
 
-  if(fstat(fd, &st) < 0){
+  if (fstat(fd, &st) < 0)
+  {
     fprintf(2, "find: cannot stat %s\n", path);
     close(fd);
     return;
   }
-    char* fileName;
-  switch(st.type){
+  char *fileName;
+  switch (st.type)
+  {
   case T_FILE:
     fileName = fmtname(path);
     printf("%s\n", fmtname(path));
     break;
 
   case T_DIR:
-    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+    {
       printf("find: path too long\n");
       break;
     }
     strcpy(buf, path);
-    p = buf+strlen(buf);
+    p = buf + strlen(buf);
     *p++ = '/';
-    while(read(fd, &de, sizeof(de)) == sizeof(de)){
-      if(de.inum == 0)
+    while (read(fd, &de, sizeof(de)) == sizeof(de))
+    {
+      if (de.inum == 0)
         continue;
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = 0;
-      if(stat(buf, &st) < 0){
+      if (stat(buf, &st) < 0)
+      {
         printf("find: cannot stat %s\n", buf);
         continue;
       }
@@ -70,76 +76,78 @@ void find(char *path, char* pattern)
       switch (st.type)
       {
       case T_FILE:
-          if (match(pattern, fileName))
-          {
-              printf("%s\n", fileName);
-          }
-          break;
+        if (match(pattern, fileName))
+        {
+          printf("%s\n", fileName);
+        }
+        break;
 
-          case T_DIR:
-              find(path, pattern);
-              break;
-          }     
+      case T_DIR:
+        if (strcmp(de.name, ".") && strcmp(de.name, "..")) // 相等的话返回0，不等返回两个最后一位的差值
+        {
+          find(buf, pattern);
+        }
+        break;
+      }
     }
     break;
   }
   close(fd);
 }
 
-
 int main(int argc, char *argv[])
 {
-    if (argc <= 2)
-    {
-        printf("usage: find [path] [expression]!\n");
-        exit();
-    }
-    char* path = argv[1];
-    char* expression = argv[2];
-    find(path, expression);
-
+  if (argc <= 2)
+  {
+    printf("usage: find [path] [expression]!\n");
     exit();
-}
+  }
+  char *path = argv[1];
+  char *expression = argv[2];
+  find(path, expression);
 
+  exit();
+}
 
 // Regexp matcher from Kernighan & Pike,
 // The Practice of Programming, Chapter 9.
 
-int matchhere(char*, char*);
-int matchstar(int, char*, char*);
+int matchhere(char *, char *);
+int matchstar(int, char *, char *);
 
-int
-match(char *re, char *text)
+int match(char *re, char *text)
 {
-  if(re[0] == '^')
-    return matchhere(re+1, text);
-  do{  // must look at empty string
-    if(matchhere(re, text))
+  if (re[0] == '^')
+    return matchhere(re + 1, text);
+  do
+  { // must look at empty string
+    if (matchhere(re, text))
       return 1;
-  }while(*text++ != '\0');
+  } while (*text++ != '\0');
   return 0;
 }
 
 // matchhere: search for re at beginning of text
 int matchhere(char *re, char *text)
 {
-  if(re[0] == '\0')
+  if (re[0] == '\0')
     return 1;
-  if(re[1] == '*')
-    return matchstar(re[0], re+2, text);
-  if(re[0] == '$' && re[1] == '\0')
+  if (re[1] == '*')
+    return matchstar(re[0], re + 2, text);
+  if (re[0] == '$' && re[1] == '\0')
     return *text == '\0';
-  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
-    return matchhere(re+1, text+1);
+  if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+    return matchhere(re + 1, text + 1);
   return 0;
 }
 
 // matchstar: search for c*re at beginning of text
 int matchstar(int c, char *re, char *text)
 {
-  do{  // a * matches zero or more instances
-    if(matchhere(re, text))
+  do
+  { // a * matches zero or more instances
+    if (matchhere(re, text))
       return 1;
-  }while(*text!='\0' && (*text++==c || c=='.'));
+  } while (*text != '\0' && (*text++ == c || c == '.'));
   return 0;
 }
