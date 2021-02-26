aedfb0379a26d8461829e4b3613b656f68d2eb25
Kevin-Mei <Mei_Yao_1@163.com>
2021-02-26 20:48:02 +0800

    update xargs.c

--- user/xargs.c
+++ user/xargs.c
@@ -7,15 +7,15 @@ int main(int argc, char *argv[])
     // 然后要从标准输入中读取数据，直到遇到\n
     char buff[1024];
     int offset = 0;
-    int len = read(0, buff+offset, sizeof(buff)-offset);
-    while(len)
+    int len = 0;
+    while((len = read(0, buff+offset, sizeof(buff)-offset)) > 0)
     {
         // 判断遇到\n
         if('\n' == buff[offset+len-1])
         {
             break;
         }
-        len = read(0, buff+offset, sizeof(buff)-offset);
+        offset+=len;
     }
     // 把buff添加进argv[], 然后exec
     // 这里抄下echo的作业，不行，应该是fork，然后子进程执行argv[1]，然后argv[2]及以后的数据+buff是新的argv
@@ -31,8 +31,8 @@ int main(int argc, char *argv[])
             argv[i] = argv[i+1];
             i++;
         }
-
-        exec(argv[1], argv[2]);
+        argv[i+1] = buff; 
+        exec(argv[1], &argv[2]);
     }else
     {
         wait();
